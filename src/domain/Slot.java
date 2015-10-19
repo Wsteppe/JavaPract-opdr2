0214effd636496200ec719cf76198e5689a99cbc
ruben <ruben@denies.info>
2015-10-19 20:46:48 +0200

    Added method placeholders

--- src/domain/Slot.java
+++ src/domain/Slot.java
@@ -1,10 +1,50 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import domain.*;
+import domain.tellers.Teller;
+
 public class Slot {
+
+	private String geheimeCode;
 
 	
 	public Slot() throws Exception
 	{
-	
 	}
+	
+	public char getWaardeTeller(int teller){
+		char character = 'F';
+		return character;
+	}
+	
+	public List<Teller> getTellers() {
+		  List<Teller> list = new ArrayList<Teller>();      
+		  return list;
+	}
+	
+	public String getSlotKombinatie(){
+		String slotKombinatie = "123";
+		return slotKombinatie;
+	}
+	
+	public void updateHuidigeWaardeTeller(int waarde){
+	}
+	
+	public int getAantalTellers(){
+		int aantalTellers = 1;
+		return aantalTellers;
+	}
+	
+	public boolean isGeheimGevonden(){
+		
+		boolean gevonden = false;
+		return gevonden;
+	}
+	
+	public void resetTellers(){
+	}
+	
 }
